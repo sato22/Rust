a636fdc0c2aeda7e0f400b4aa693f410925d6ec9
sato <53112714+sato22@users.noreply.github.com>
2020-10-12 14:54:02 +0900

    Update main.rs

--- pointer/main.rs
+++ pointer/main.rs
@@ -1,8 +1,11 @@
 fn main() {
 	let k:i32 = 12345;
 	
+	// 生ポインタを取得するには、参照からas *const T（イミュータブルの場合）またはas *mut T（ミュータブルの場合）でキャスト 
 	let p: *const i32 = &k as *const i32;
 	let t = unsafe{*p};
 
+	// ポインタを表示するために使用可能なポインタが他のデフォルトのフォーマッタ　{:p}
 	println!( "p={:p}, t={}", p, t);
+	
 }
